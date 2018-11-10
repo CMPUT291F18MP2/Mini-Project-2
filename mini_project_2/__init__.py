2ff11933e2b7c3a19c3830ea5688772659fc45aa
Ryan Furrer <rfurrer@ualberta.ca>
2018-11-09 19:50:31 -0700

    Removed mp1 specific code

--- mini_project_2/__init__.py
+++ mini_project_2/__init__.py
@@ -10,22 +10,7 @@ sharing applications.
 Scripts:
  + :mod:`.__main__` - argparse entry point
 Modules:
- + :mod:`.book_member` -
- + :mod:`.cancel_booking` -
- + :mod:`.common` - common functionality used in mini-project-1
- + :mod:`.delete_request` -
- + :mod:`.list_bookings` -
- + :mod:`.list_requests` -
- + :mod:`.loginsession` - login session object definition
- + :mod:`.logout` -
- + :mod:`.offer_ride` -
- + :mod:`.post_request` -
- + :mod:`.register` - mini-project-1 member registration
- + :mod:`.search_ride_requests` -
- + :mod:`.search_rides` -
- + :mod:`.select_ride_request` -
- + :mod:`.shell` - command shell
- + :mod:`.show_inbox` -
+ + :mod:`.common` - common functionality used in mini-project-2
 """
 
 __version__ = (0, 0, 0)
