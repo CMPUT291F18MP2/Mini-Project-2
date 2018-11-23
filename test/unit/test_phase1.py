3e9d85c9aad79b2c7060822b488f5c8b69f62dec
Ryan Furrer <rfurrer@ualberta.ca>
2018-11-23 11:42:20 -0700

    Refactoring

--- test/unit/test_phase1.py
+++ test/unit/test_phase1.py
@@ -1,9 +1,8 @@
 import os
 
-import mini_project_2
+from mini_project_2.common import ads_file, prices_file, pdates_file, terms_file, MINI_PROJECT_2_PATH
 from mini_project_2.phase1 import is_ad_line, generate_data_files
 
-MINI_PROJECT_2_PATH = os.path.dirname(os.path.realpath(mini_project_2.__file__))
 small_input_file = os.path.join(MINI_PROJECT_2_PATH, "smallTestData/10.txt")
 small_ads_file = os.path.join(MINI_PROJECT_2_PATH, "smallTestData/10-ads.txt")
 small_terms_file = os.path.join(MINI_PROJECT_2_PATH, "smallTestData/10-terms.txt")
@@ -15,11 +14,6 @@ med_ads_file = os.path.join(MINI_PROJECT_2_PATH, "1000TestData/1k-ads.txt")
 med_terms_file = os.path.join(MINI_PROJECT_2_PATH, "1000TestData/1k-terms.txt")
 med_pdates_file = os.path.join(MINI_PROJECT_2_PATH, "1000TestData/1k-pdates.txt")
 med_prices_file = os.path.join(MINI_PROJECT_2_PATH, "1000TestData/1k-prices.txt")
-
-ads_file = os.path.join(MINI_PROJECT_2_PATH, "data/ads.txt")
-prices_file = os.path.join(MINI_PROJECT_2_PATH, "data/prices.txt")
-pdates_file = os.path.join(MINI_PROJECT_2_PATH, "data/pdates.txt")
-terms_file = os.path.join(MINI_PROJECT_2_PATH, "data/terms.txt")
 
 
 def test_is_ad_line():
