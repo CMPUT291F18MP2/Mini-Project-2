c3d866c69d24a796ad331e591a400680e43a65f9
Tim Tran <ttran1@cs.ualberta.ca>
2018-11-23 10:50:14 -0700

    Add sort_all function

--- mini_project_2/phase2.py
+++ mini_project_2/phase2.py
@@ -1,12 +1,19 @@
 import subprocess
+import os
+import mini_project_2
 
+MINI_PROJECT_2_PATH = os.path.dirname(os.path.realpath(mini_project_2.__file__))
+ads_file = os.path.join(MINI_PROJECT_2_PATH, "data/ads.txt")
+terms_file = os.path.join(MINI_PROJECT_2_PATH, "data/terms.txt")
+pdates_file = os.path.join(MINI_PROJECT_2_PATH, "data/pdates.txt")
+prices_file = os.path.join(MINI_PROJECT_2_PATH, "data/prices.txt")
 
 # command: sort inputfile.txt -u
 def sort_all():
-    sortData("ads.txt", "ads.txt")
-    sortData("pdates.txt", "pdates.txt")
-    sortData("prices.txt", "prices.txt")
-    sortData("terms.txt", "terms.txt")
+    sort_data(ads_file, ads_file)
+    sort_data(terms_file, terms_file)
+    sort_data(pdates_file, pdates_file)
+    sort_data(prices_file, prices_file)
 
 def sort_data(infile, outfile):
     command = ["sort", "-o", outfile, infile, "-u"]
--- test/unit/test_phase2.py
+++ test/unit/test_phase2.py
@@ -7,11 +7,13 @@ from mini_project_2.phase2 import sort_data, sort_all
 
 MINI_PROJECT_2_PATH = os.path.dirname(os.path.realpath(mini_project_2.__file__))
 ads_infile = os.path.join(MINI_PROJECT_2_PATH, "data/ads.txt")
-ads_outfile = os.path.join(MINI_PROJECT_2_PATH, "data/sorted/ads.txt")
 
 def test_sort_ads():
     sort_data(ads_infile, ads_infile)
 
+def test_sort_all():
+    sort_all()
+
 if __name__ == "__main__":
-    test_sort_ads()
+    test_sort_all()
     pass
