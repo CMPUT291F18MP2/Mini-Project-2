2dbe5f29fdfcd9dc24540b567247fa9a6dd5a99d
Tim Tran <ttran1@cs.ualberta.ca>
2018-11-24 16:32:27 -0700

    implement validate_input() and write more tests

--- mini_project_2/input_parser.py
+++ mini_project_2/input_parser.py
@@ -1,5 +1,27 @@
 """ Class that validates and parses queries """
 
+import re
+
+alphanumeric = r"[0-9a-zA-Z_-]"
+numeric = r"[0-9]"
+date = r"[0-9]{4}/[0-9]{2}/[0-9]{2}"
+date_prefix = r"(date)\s*(>=|<=|=|>|<)"
+date_query = date_prefix + r"\s*" + date
+price = numeric + r"+"
+price_prefix = r"(price)\s*(>=|<=|=|>|<)"
+price_query = price_prefix + r"\s*" + price
+location = alphanumeric + r"+"
+location_prefix = r"(location)\s*="
+location_query = location_prefix + r"\s*" + location
+cat = alphanumeric + r"+"
+cat_prefix = r"(cat)\s*="
+cat_query = cat_prefix + r"\s*" + cat
+term = alphanumeric + r"+"
+term_suffix = r"%"
+term_query = term + term_suffix + r"|" + term
+expression = r"(" + date_query + r"|" + price_query + r"|" + location_query + r"|" + cat_query + r"|" + term_query + r")"
+query = r"\s*" + expression + r"(\s+" + expression + r")*" + r"\s*"
+
 class InputParser():
 
     def __init__(self):
@@ -8,14 +30,19 @@ class InputParser():
     def __log_setup(self):
         return None
 
-    def validate_input(self, input):
+    def validate_query(self, requested_query):
         """ Validates user input
             Returns true if valid.
             Returns false otherwise
         """
-        return False
+        # Add ^ and $ to the pattern to match the whole string.
+        pattern = r"^" + query + r"$"
+        if re.match(pattern, requested_query, re.I):
+            return True
+        else:
+            return False
 
-    def parse_input(self, input):
+    def parse_input(self, query):
         """ Parses the user input and returns a
             dictionary of the requested searches.
         """
--- test/unit/test_input_parser.py
+++ test/unit/test_input_parser.py
@@ -6,16 +6,118 @@ from mini_project_2.input_parser import InputParser
 def test_validate_input_1():
     ip = InputParser()
     query = "camera"
-    val = ip.validate_input(query)
+    val = ip.validate_query(query)
+    assert val == True
+
+def test_validate_input_2():
+    ip = InputParser()
+    query = "camera%"
+    val = ip.validate_query(query)
+    assert val == True
+
+def test_validate_input_3():
+    ip = InputParser()
+    query = "date <= 2018/11/05"
+    val = ip.validate_query(query)
+    assert val == True
+
+def test_validate_input_4():
+    ip = InputParser()
+    query = "date > 2018/11/05"
+    val = ip.validate_query(query)
+    assert val == True
+
+def test_validate_input_5():
+    ip = InputParser()
+    query = "price < 20"
+    val = ip.validate_query(query)
+    assert val == True
+
+def test_validate_input_6():
+    ip = InputParser()
+    query = "price >= 20"
+    val = ip.validate_query(query)
+    assert val == True
+
+def test_validate_input_7():
+    ip = InputParser()
+    query = "location=edmonton date=2018/11/07"
+    val = ip.validate_query(query)
+    assert val == True
+
+def test_validate_input_8():
+    ip = InputParser()
+    query = "cat=art-collectibles camera"
+    val = ip.validate_query(query)
     assert val == True
 
 def test_validate_input_9():
     ip = InputParser()
     query = "camera date>=2018/11/05 date<=2018/11/07 price > 20 price < 40"
-    val = ip.validate_input(query)
+    val = ip.validate_query(query)
     assert val == True
 
-def test_parse_input_9():
+def test_validate_input_10():
+    ip = InputParser()
+    query = " "
+    val = ip.validate_query(query)
+    assert val == False
+
+def test_validate_input_11():
+    ip = InputParser()
+    query = r"hello []"
+    val = ip.validate_query(query)
+    assert val == False
+
+def test_validate_input_12():
+    ip = InputParser()
+    query = r"      camera date>=2018    /11/05 "
+    val = ip.validate_query(query)
+    assert val == False
+
+def test_validate_input_13():
+    ip = InputParser()
+    query = r"3413"
+    val = ip.validate_query(query)
+    assert val == True
+
+def test_validate_input_14():
+    ip = InputParser()
+    query = r"date >=      2018/11/05    cat=yolo"
+    val = ip.validate_query(query)
+    assert val == True
+
+def test_validate_input_15():
+    ip = InputParser()
+    query = r"date=hello"
+    val = ip.validate_query(query)
+    assert val == False
+
+def test_validate_input_16():
+    ip = InputParser()
+    query = r" dafjeoj      date=30"
+    val = ip.validate_query(query)
+    assert val == False
+
+def test_validate_input_17():
+    ip = InputParser()
+    query = r"weeueueeee hello"
+    val = ip.validate_query(query)
+    assert val == True
+
+def test_validate_input_18():
+    ip = InputParser()
+    query = r"price <=> 50"
+    val = ip.validate_query(query)
+    assert val == False
+
+def test_validate_input_19():
+    ip = InputParser()
+    query = r"50 < price <60"
+    val = ip.validate_query(query)
+    assert val == False
+
+def test_parse_input_1():
     ip = InputParser()
     query = "camera"
     searches_actual = ip.parse_input(query)
