200dda9c48f120770c8c2993d4845e17fd09c2cb
Tim Tran <ttran1@cs.ualberta.ca>
2018-11-25 00:24:56 -0700

    Add parsing function for search type and unit tests

--- mini_project_2/input_parser.py
+++ mini_project_2/input_parser.py
@@ -7,17 +7,17 @@ numeric = r"[0-9]"
 date = r"([0-9]{4}/[0-9]{2}/[0-9]{2})"
 date_prefix = r"(date)\s*(>=|<=|=|>|<)"
 date_query = date_prefix + r"\s*" + date
-price = numeric + r"+"
+price = r"(" + numeric + r"+)"
 price_prefix = r"(price)\s*(>=|<=|=|>|<)"
 price_query = price_prefix + r"\s*" + price
-location = alphanumeric + r"+"
-location_prefix = r"(location)\s*="
+location = r"(" + alphanumeric + r"+)"
+location_prefix = r"(location)\s*(=)"
 location_query = location_prefix + r"\s*" + location
-cat = alphanumeric + r"+"
-cat_prefix = r"(cat)\s*="
+cat = r"(" + alphanumeric + r"+)"
+cat_prefix = r"(cat)\s*(=)"
 cat_query = cat_prefix + r"\s*" + cat
-term = alphanumeric + r"+"
-term_suffix = r"%"
+term = r"(" + alphanumeric + r"+)"
+term_suffix = r"(%)"
 term_query = term + term_suffix + r"|" + term
 expression = r"(" + date_query + r"|" + price_query + r"|" + location_query + r"|" + cat_query + r"|" + term_query + r")"
 query = r"\s*" + expression + r"(\s+" + expression + r")*" + r"\s*"
@@ -75,6 +75,106 @@ class InputParser():
         else:
             search_dict['date'] = operator_value_pairs
 
-        # TODO: remove dates from input query
         input_query = re.sub(date_query, '', input_query, flags=re.I)
+        return input_query
+
+    def parse_for_price(self, input_query, search_dict):
+        """ Parses input_query for a price search.
+            Adds any price searches to search_dict.
+            Returns the input_query with prices removed.
+        """
+        operator_value_pairs = list()
+        price_search_patterns = re.findall(price_query, input_query, re.I)
+
+        # price_search_patterns = a list of ("price", operator, value)
+        for item in price_search_patterns:
+            operator_value_pairs.append((item[1], item[2]))
+
+        if 'price' in search_dict:
+            for item in operator_value_pairs:
+                search_dict['price'].append(item)
+        else:
+            search_dict['price'] = operator_value_pairs
+
+        # remove prices from the query and return it
+        input_query = re.sub(price_query, '', input_query, flags=re.I)
+        return input_query
+
+    def parse_for_location(self, input_query, search_dict):
+        """ Parses input_query for a location search.
+            Adds any location searches to search_dict.
+            Returns the input_query with locations removed.
+        """
+        operator_value_pairs = list()
+        location_search_patterns = re.findall(location_query, input_query, re.I)
+
+        # location_search_patterns = a list of ("location", "=", value)
+        for item in location_search_patterns:
+            operator_value_pairs.append((item[1], item[2]))
+
+        if 'location' in search_dict:
+            for item in operator_value_pairs:
+                search_dict['location'].append(item)
+        else:
+            search_dict['location'] = operator_value_pairs
+
+        # remove locations from the query and return it
+        input_query = re.sub(location_query, '', input_query, flags=re.I)
+        return input_query
+
+    def parse_for_category(self, input_query, search_dict):
+        """ Parses input_query for a category search.
+            Adds any category searches to search_dict.
+            Returns the input_query with categories removed.
+        """
+        operator_value_pairs = list()
+        category_search_patterns = re.findall(cat_query, input_query, re.I)
+
+        # category_search_patterns = a list of ("cat", "=", value)
+        for item in category_search_patterns:
+            operator_value_pairs.append((item[1], item[2]))
+
+        if 'category' in search_dict:
+            for item in operator_value_pairs:
+                search_dict['category'].append(item)
+        else:
+            search_dict['category'] = operator_value_pairs
+
+        # remove categorys from the query and return it
+        input_query = re.sub(cat_query, '', input_query, flags=re.I)
+        return input_query
+
+    def parse_for_keyword(self, input_query, search_dict):
+        """ Parses input_query for a keyword search.
+            Adds any keyword searches to search_dict.
+            Returns the input_query with keywords removed.
+        """
+        # strip all other query types from the input_query
+        stripped_query = re.sub(date_query, '', input_query, flags=re.I)
+        stripped_query = re.sub(price_query, '', stripped_query, flags=re.I)
+        stripped_query = re.sub(location_query, '', stripped_query, flags=re.I)
+        stripped_query = re.sub(cat_query, '', stripped_query, flags=re.I)
+        keywords = stripped_query.split()
+
+        operator_value_pairs = list()
+        keyword_search_patterns = re.findall(term_query, stripped_query, re.I)
+
+        # keyword_search_patterns = a list of (term, "%", "") or ("", "", term)
+        for item in keyword_search_patterns:
+            if item[1]:
+                operator_value_pairs.append((item[1], item[0]))
+            elif item[2]:
+                operator_value_pairs.append(("=", item[2]))
+            else:
+                print("parse_for_keyword got messed up")
+
+        if 'keyword' in search_dict:
+            for item in operator_value_pairs:
+                search_dict['keyword'].append(item)
+        else:
+            search_dict['keyword'] = operator_value_pairs
+
+        # remove keywords from the original query and return it
+        for item in keywords:
+            input_query = input_query.replace(item, "")
         return input_query
--- test/unit/test_input_parser.py
+++ test/unit/test_input_parser.py
@@ -141,6 +141,129 @@ def test_parse_for_date_3():
     assert search_dict == {"date": [(">=", "2018/11/05"), ("=", "2025/01/01")]}
     assert new_query == r"camera "
 
+def test_parse_for_price_1():
+    ip = InputParser()
+    query = r"price < 20"
+    search_dict = dict()
+    new_query = ip.parse_for_price(query, search_dict)
+    assert search_dict == {"price": [("<", "20")]}
+    assert new_query == r""
+
+def test_parse_for_price_2():
+    ip = InputParser()
+    query = r"price < 20 price=40"
+    search_dict = dict()
+    new_query = ip.parse_for_price(query, search_dict)
+    assert search_dict == {"price": [("<", "20"), ("=", "40")]}
+    assert new_query == r" "
+
+def test_parse_for_price_3():
+    ip = InputParser()
+    query = r"price >=420 have you ever heard the tragedy of dark plagueis the wise"
+    search_dict = {"price": [("<", "20")]}
+    new_query = ip.parse_for_price(query, search_dict)
+    assert search_dict == {"price": [("<", "20"), (">=", "420")]}
+    assert new_query == r" have you ever heard the tragedy of dark plagueis the wise"
+
+def test_parse_for_location_1():
+    ip = InputParser()
+    query = r"location=edmonton date=2018/11/07"
+    search_dict = dict()
+    new_query = ip.parse_for_location(query, search_dict)
+    assert search_dict == {"location": [("=", "edmonton")]}
+    assert new_query == r" date=2018/11/07"
+
+def test_parse_for_location_2():
+    ip = InputParser()
+    query = r"location=coruscant location=tatooine"
+    search_dict = dict()
+    new_query = ip.parse_for_location(query, search_dict)
+    assert search_dict == {"location": [("=", "coruscant"), ("=", "tatooine")]}
+    assert new_query == r" "
+
+def test_parse_for_location_3():
+    ip = InputParser()
+    query = r"location=coruscant date=2019/11/11 location=tatooine"
+    search_dict = {"location": [("=", "edmonton")]}
+    new_query = ip.parse_for_location(query, search_dict)
+    assert search_dict == {"location": [("=", "edmonton"), ("=", "coruscant"), ("=", "tatooine")]}
+    assert new_query == r" date=2019/11/11 "
+
+def test_parse_for_location_4():
+    ip = InputParser()
+    query = r"date=2019/11/11 location=tatooine"
+    search_dict = {"price": [("=", "30")]}
+    new_query = ip.parse_for_location(query, search_dict)
+    assert search_dict == {
+        "price": [("=", "30")],
+        "location": [("=", "tatooine")]
+        }
+    assert new_query == r"date=2019/11/11 "
+
+def test_parse_for_category_1():
+    ip = InputParser()
+    query = r"cat=art-collectibles camera"
+    search_dict = dict()
+    new_query = ip.parse_for_category(query, search_dict)
+    assert search_dict == {"category": [("=", "art-collectibles")]}
+    assert new_query == r" camera"
+
+def test_parse_for_category_2():
+    ip = InputParser()
+    query = r"cat=not-stories-jedi-would-tell-you camera cat=art-collectibles"
+    search_dict = dict()
+    new_query = ip.parse_for_category(query, search_dict)
+    assert search_dict == {"category": [("=", "not-stories-jedi-would-tell-you"), ("=", "art-collectibles")]}
+    assert new_query == r" camera "
+
+def test_parse_for_category_3():
+    ip = InputParser()
+    query = r"cat=kenobi location=dagobah"
+    search_dict = {"price": [(">", "500")], "category": [("=", "general")]}
+    new_query = ip.parse_for_category(query, search_dict)
+    assert search_dict == {
+        "price": [(">", "500")],
+        "category": [("=", "general"), ("=", "kenobi")]}
+    assert new_query == r" location=dagobah"
+
+def test_parse_for_keyword_1():
+    ip = InputParser()
+    query = r"camera location=my-house"
+    search_dict = dict()
+    new_query = ip.parse_for_keyword(query, search_dict)
+    assert search_dict == {"keyword": [("=", "camera")]}
+    assert new_query == r" location=my-house"
+
+def test_parse_for_keyword_2():
+    ip = InputParser()
+    query = r"camera%"
+    search_dict = dict()
+    new_query = ip.parse_for_keyword(query, search_dict)
+    assert search_dict == {"keyword": [("%", "camera")]}
+    assert new_query == r""
+
+def test_parse_for_keyword_3():
+    ip = InputParser()
+    query = r"midichlorians cat=starwars darth%"
+    search_dict = dict()
+    new_query = ip.parse_for_keyword(query, search_dict)
+    assert search_dict == {"keyword": [("=", "midichlorians"), ("%", "darth")]}
+    assert new_query == r" cat=starwars "
+
+def test_parse_for_keyword_4():
+    ip = InputParser()
+    query = r"location=deathstar ihatesand%"
+    search_dict = {
+        "keyword": [("=", "prequelsarethebest")],
+        "date": [(">=", "2018/05/03")]
+    }
+    new_query = ip.parse_for_keyword(query, search_dict)
+    assert search_dict == {
+        "keyword": [("=", "prequelsarethebest"), ("%", "ihatesand")],
+        "date": [(">=", "2018/05/03")]
+    }
+    assert new_query == r"location=deathstar "
+
 def test_parse_input_1():
     ip = InputParser()
     query = "camera"
