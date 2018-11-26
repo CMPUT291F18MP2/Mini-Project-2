13be9e3d19cd2b100f1943dd5e8c40e1cc231ca0
Ryan Furrer <rfurrer@ualberta.ca>
2018-11-26 00:05:11 -0700

    phase3 unit test stubs

---
+++ test/unit/test_phase3.py
@@ -0,0 +1,113 @@
+from mini_project_2.phase3 import AdsDatabase
+
+
+def test_execute1():
+    with AdsDatabase() as ads_database:
+        pass
+    pass
+
+
+def test_execute2():
+    with AdsDatabase() as ads_database:
+        pass
+    pass
+
+
+def test_execute3():
+    with AdsDatabase() as ads_database:
+        pass
+    pass
+
+
+def test_execute4():
+    with AdsDatabase() as ads_database:
+        pass
+    pass
+
+
+def test_execute5():
+    with AdsDatabase() as ads_database:
+        pass
+    pass
+
+
+def test_execute6():
+    with AdsDatabase() as ads_database:
+        pass
+    pass
+
+
+def test_execute7():
+    with AdsDatabase() as ads_database:
+        pass
+    pass
+
+
+def test_phase3():
+    pass
+
+
+def test_parse_date():
+    pass
+
+
+def test_parse_price_range():
+    pass
+
+
+def test_parse_date_range():
+    pass
+
+
+def test_change_mode():
+    with AdsDatabase() as ads_database:
+        pass
+    pass
+
+
+def test_get_matching_terms():
+    with AdsDatabase() as ads_database:
+        pass
+    pass
+
+
+def test_get_matching_prices():
+    with AdsDatabase() as ads_database:
+        pass
+    pass
+
+
+def test_get_matching_dates():
+    with AdsDatabase() as ads_database:
+        pass
+    pass
+
+
+def test_print_matching_ads():
+    with AdsDatabase() as ads_database:
+        pass
+    pass
+
+
+def test_merge_results():
+    with AdsDatabase() as ads_database:
+        pass
+    pass
+
+
+def test_():
+    with AdsDatabase() as ads_database:
+        pass
+    pass
+
+
+def test_print_results():
+    with AdsDatabase() as ads_database:
+        pass
+    pass
+
+
+def test_print_one_result():
+    with AdsDatabase() as ads_database:
+        pass
+    pass
